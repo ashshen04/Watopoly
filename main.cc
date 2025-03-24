b9afdff05605505111c25ac4728adf5e3b35f5c3
ashshen04 <ashely.shenn@gmail.com>
2025-03-24 19:43:44 -0400

    updated main.cc

---
+++ main.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main(int argc, char*[] argv) {
+    const int MIN_PLAYER = 2;
+    const int MAX_PLAYER = 8;
+    const int STARTING_MONEY = 1500;
+
+    // New game is created
+    if (argv[0] == "./watopoly") {
+
+    }
+    // Loading previous game (-load file)
+
+    // Testing Mode (-testing)
+
+}
